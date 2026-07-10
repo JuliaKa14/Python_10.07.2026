0309e6cacb56a9e6c9ec4e9ed3e7d0b50f30669f
JuliaKa14 <jtomorrow49@gmail.com>
2026-07-11 01:47:07 +0300

    Домашняя работа 9

---
+++ lesson_09/config.py
@@ -0,0 +1,3 @@
+DB_URL = (
+    "postgresql://postgres:Bashmak2414@localhost:5432/QA" # Для запуска тестов необходимо указать пароль пользователя PostgreSQL в файле config.py. перед сдачей пароль изменить на YOUR_PASSWORD
+)
